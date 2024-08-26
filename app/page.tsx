7925109f9b91109344d76c6aa44bceffefbefe9d
Mohammad Reduwan <ridwan0gts@gmail.com>
2024-08-27 02:33:35 +0800

    Completed configuration of sentry

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
+import dynamic from "next/dynamic";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="relative bg-black-100 justify-center items-center flex-col overflow-hidden mx-auto am:px-10 px-5">
       <div className="max-w-7xl w-full">
@@ -16,4 +16,6 @@ export default function Home() {
       </div>
     </main>
   );
-}
+};
+
+export default dynamic(() => Promise.resolve(Home), { ssr: false });
